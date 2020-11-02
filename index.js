048207d849f33af1d91e70219e9fd37fea40bf06
witty-abstraction-1855 <merve.kastan4@gmail.com>
2020-11-02 20:18:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,19 +16,19 @@ function nowServing(katzDeliLine){
   }
 }
 
-function currentLine(){
+function currentLine(katzDeliLine){
+  var line= []
   if (katzDeliLine.length=== 0){
     return "The line is currently empty."
   }
   else {
-    for (var i=0; katzDeliLine.length;i++) {
-      return `The line is currently: 1. ${katzDeliLine} , ${katzDeliLine}`
+    let i = 0;
+  while (i < katzDeliLine.length) {
+    line.push(` `+[i+1]+`. `  + katzDeliLine[i])
+    i++;
+  }
     }
+    return(`The line is currently:` + line);
   }
 
 
-
-
-
-
-}
